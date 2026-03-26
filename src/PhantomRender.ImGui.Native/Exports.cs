4c69c4b0f664ff68eba4aac952cdc0936c257cf1
Destroyer <32405118+DestroyerDarkNess@users.noreply.github.com>
2026-03-25 20:50:42 -0400

    Update Internal Overlay

--- src/PhantomRender.ImGui.Native/Exports.cs
+++ src/PhantomRender.ImGui.Native/Exports.cs
@@ -4,10 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
 using PhantomRender.Core;
-using PhantomRender.Core.Hooks.Graphics;
-using PhantomRender.Core.Hooks.Graphics.OpenGL;
 using PhantomRender.ImGui;
-using PhantomRender.ImGui.Core;
 using PhantomRender.ImGui.Core.Renderers;
 
 namespace PhantomRender.ImGui.Native
@@ -22,8 +19,6 @@ namespace PhantomRender.ImGui.Native
         private static int _shutdownRequested;
         private static InternalOverlay _overlay;
         private static UI _ui;
-        private static DirectX9Hook _directX9Hook;
-        private static OpenGLHook _openGLHook;
 
         [UnmanagedCallersOnly(EntryPoint = "DllMain", CallConvs = new[] { typeof(CallConvStdcall) })]
         public static unsafe bool DllMain(IntPtr hModule, uint ul_reason_for_call, IntPtr lpReserved)
@@ -84,19 +79,32 @@ namespace PhantomRender.ImGui.Native
         private static bool InitializeInternal()
         {
             GraphicsApi graphicsApi = WaitForSupportedGraphicsApi(TimeSpan.FromSeconds(15));
-            return graphicsApi switch
+            if (graphicsApi == GraphicsApi.Unknown)
             {
-                GraphicsApi.DirectX9 => InitializeDirectX9(),
-                GraphicsApi.OpenGL => InitializeOpenGL(),
-                _ => false,
-            };
-
-            var resolver = new DependencyResolver();
-            if (!resolver.LoadDependencies(_hModule))
-            {
-                Console.WriteLine("[PhantomRender] Failed to load native dependencies.");
                 return false;
             }
+
+            RendererBase renderer = CreateInternalRenderer(graphicsApi);
+            var overlay = new InternalOverlay(renderer)
+            {
+                DependencyModuleHandle = _hModule,
+            };
+
+            var ui = new UI(overlay);
+            if (!overlay.Start())
+            {
+                ui.Dispose();
+                overlay.Dispose();
+                return false;
+            }
+
+            lock (SyncRoot)
+            {
+                _overlay = overlay;
+                _ui = ui;
+            }
+
+            return true;
         }
 
         private static GraphicsApi WaitForSupportedGraphicsApi(TimeSpan timeout)
@@ -120,223 +128,57 @@ namespace PhantomRender.ImGui.Native
             return GraphicsApi.Unknown;
         }
 
-        private static bool InitializeDirectX9()
+        private static RendererBase CreateInternalRenderer(GraphicsApi graphicsApi)
         {
-            var renderer = new DirectX9Renderer();
-            var overlay = new InternalOverlay(renderer);
-            var ui = new UI(overlay);
-
-            IntPtr deviceAddress = DirectX9Hook.GetDeviceAddress();
-            if (deviceAddress == IntPtr.Zero)
+            switch (graphicsApi)
             {
-                ui.Dispose();
-                overlay.Dispose();
-                return false;
+                case GraphicsApi.DirectX9:
+                    return new DirectX9Renderer();
+
+                case GraphicsApi.OpenGL:
+                    return new OpenGLRenderer();
+
+                default:
+                    throw new NotSupportedException($"{graphicsApi.ToDisplayName()} does not have an internal native host.");
             }
-
-            DX9HookFlags flags = renderer.InitializationEndpoint == DirectX9InitializationEndpoint.EndScene
-                ? DX9HookFlags.EndScene | DX9HookFlags.Reset
-                : DX9HookFlags.Present | DX9HookFlags.Reset;
-
-            var directX9Hook = new DirectX9Hook(deviceAddress, flags);
-            directX9Hook.OnBeforeReset += HandleDirectX9BeforeReset;
-            directX9Hook.OnAfterReset += HandleDirectX9AfterReset;
-
-            if (renderer.InitializationEndpoint == DirectX9InitializationEndpoint.EndScene)
-            {
-                directX9Hook.OnEndScene += HandleDirectX9EndScene;
-            }
-            else
-            {
-                directX9Hook.OnPresent += HandleDirectX9Present;
-            }
-
-            lock (SyncRoot)
-            {
-                _overlay = overlay;
-                _ui = ui;
-                _directX9Hook = directX9Hook;
-            }
-
-            directX9Hook.Enable();
-            return true;
-        }
-
-        private static bool InitializeOpenGL()
-        {
-            var overlay = new InternalOverlay(new OpenGLRenderer());
-            var ui = new UI(overlay);
-            var openGLHook = new OpenGLHook();
-            openGLHook.OnSwapBuffers += HandleOpenGLSwapBuffers;
-
-            lock (SyncRoot)
-            {
-                _overlay = overlay;
-                _ui = ui;
-                _openGLHook = openGLHook;
-            }
-
-            openGLHook.Enable();
-            return true;
-        }
-
-        private static void HandleDirectX9Present(IntPtr device, IntPtr sourceRect, IntPtr destRect, IntPtr hDestWindowOverride, IntPtr dirtyRegion)
-        {
-            RenderDirectX9Frame(device, ResolveDirectX9WindowHandle(hDestWindowOverride));
-        }
-
-        private static void HandleDirectX9EndScene(IntPtr device)
-        {
-            RenderDirectX9Frame(device, ResolveDirectX9WindowHandle(IntPtr.Zero));
-        }
-
-        private static void RenderDirectX9Frame(IntPtr device, IntPtr windowHandle)
-        {
-            InternalOverlay overlay = _overlay;
-            if (overlay == null || windowHandle == IntPtr.Zero)
-            {
-                return;
-            }
-
-            if (!overlay.Renderer.IsInitialized && !overlay.Initialize(device, windowHandle))
-            {
-                return;
-            }
-
-            overlay.BeginFrame();
-            overlay.RenderFrame();
-
-            if (_ui != null && _ui.ShutdownRequested)
-            {
-                RequestShutdown();
-            }
-        }
-
-        private static void HandleDirectX9BeforeReset(IntPtr device, PhantomRender.Core.Native.Direct3D9.D3DPRESENT_PARAMETERS presentParameters)
-        {
-            _overlay?.OnLostDevice();
-        }
-
-        private static void HandleDirectX9AfterReset(IntPtr device, PhantomRender.Core.Native.Direct3D9.D3DPRESENT_PARAMETERS presentParameters)
-        {
-            _overlay?.OnResetDevice();
-        }
-
-        private static void HandleOpenGLSwapBuffers(IntPtr hdc)
-        {
-            InternalOverlay overlay = _overlay;
-            if (overlay == null)
-            {
-                return;
-            }
-
-            IntPtr windowHandle = WindowFromDC(hdc);
-            if (windowHandle == IntPtr.Zero)
-            {
-                windowHandle = ResolveMainWindowHandle();
-            }
-
-            if (windowHandle == IntPtr.Zero)
-            {
-                return;
-            }
-
-            if (!overlay.Renderer.IsInitialized && !overlay.Initialize(hdc, windowHandle))
-            {
-                return;
-            }
-
-            overlay.BeginFrame();
-            overlay.RenderFrame();
-
-            if (_ui != null && _ui.ShutdownRequested)
-            {
-                RequestShutdown();
-            }
-        }
-
-        private static IntPtr ResolveDirectX9WindowHandle(IntPtr hDestWindowOverride)
-        {
-            if (hDestWindowOverride != IntPtr.Zero && IsWindow(hDestWindowOverride))
-            {
-                return hDestWindowOverride;
-            }
-
-            return ResolveMainWindowHandle();
-        }
-
-        private static IntPtr ResolveMainWindowHandle()
-        {
-            try
-            {
-                IntPtr mainWindow = Process.GetCurrentProcess().MainWindowHandle;
-                if (mainWindow != IntPtr.Zero && IsWindow(mainWindow))
-                {
-                    return mainWindow;
-                }
-            }
-            catch
-            {
-            }
-
-            IntPtr foregroundWindow = GetForegroundWindow();
-            if (foregroundWindow != IntPtr.Zero && IsWindow(foregroundWindow))
-            {
-                return foregroundWindow;
-            }
-
-            return IntPtr.Zero;
         }
 
         private static void RequestShutdown()
         {
             Interlocked.Exchange(ref _shutdownRequested, 1);
+            _overlay?.RequestShutdown();
         }
 
         private static bool IsShutdownRequested()
         {
-            return Volatile.Read(ref _shutdownRequested) != 0 || (_ui != null && _ui.ShutdownRequested);
+            UI ui = _ui;
+            if (ui != null && ui.ShutdownRequested)
+            {
+                _overlay?.RequestShutdown();
+            }
+
+            return Volatile.Read(ref _shutdownRequested) != 0 ||
+                   (ui != null && ui.ShutdownRequested) ||
+                   (_overlay != null && _overlay.ShutdownRequested);
         }
 
         private static void ShutdownInternal()
         {
-            DirectX9Hook directX9Hook = null;
-            OpenGLHook openGLHook = null;
             UI ui = null;
             InternalOverlay overlay = null;
 
             lock (SyncRoot)
             {
-                if (_overlay == null && _ui == null && _directX9Hook == null && _openGLHook == null)
+                if (_overlay == null && _ui == null)
                 {
                     return;
                 }
 
-                directX9Hook = _directX9Hook;
-                openGLHook = _openGLHook;
                 ui = _ui;
                 overlay = _overlay;
 
-                _directX9Hook = null;
-                _openGLHook = null;
                 _ui = null;
                 _overlay = null;
-            }
-
-            try
-            {
-                directX9Hook?.Dispose();
-            }
-            catch
-            {
-            }
-
-            try
-            {
-                openGLHook?.Dispose();
-            }
-            catch
-            {
             }
 
             try
@@ -366,15 +208,5 @@ namespace PhantomRender.ImGui.Native
         [DllImport("kernel32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool CloseHandle(IntPtr hObject);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr WindowFromDC(IntPtr hdc);
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool IsWindow(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetForegroundWindow();
     }
 }
--- src/PhantomRender.ImGui/InternalOverlay.cs
+++ src/PhantomRender.ImGui/InternalOverlay.cs
@@ -1,11 +1,22 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using PhantomRender.Core;
+using PhantomRender.Core.Hooks.Graphics;
+using PhantomRender.Core.Hooks.Graphics.OpenGL;
 using PhantomRender.ImGui.Core;
 using PhantomRender.ImGui.Core.Renderers;
-using PhantomRender.Core;
 
 namespace PhantomRender.ImGui
 {
     public sealed class InternalOverlay : Overlay
     {
+        private DirectX9Hook _directX9Hook;
+        private OpenGLHook _openGLHook;
+        private int _shutdownRequested;
+        private bool _disposed;
+
         public InternalOverlay(GraphicsApi graphicsApi)
             : this(CreateDefaultRenderer(graphicsApi))
         {
@@ -14,10 +25,58 @@ namespace PhantomRender.ImGui
         public InternalOverlay(RendererBase renderer)
             : base(renderer)
         {
-            Renderer = renderer ?? throw new System.ArgumentNullException(nameof(renderer));
+            Renderer = renderer ?? throw new ArgumentNullException(nameof(renderer));
         }
 
         public RendererBase Renderer { get; }
+
+        public bool AutoLoadDependencies { get; set; } = true;
+
+        public IntPtr DependencyModuleHandle { get; set; }
+
+        public DX9HookFlags? DirectX9HookFlagsOverride { get; set; }
+
+        public Func<IntPtr> WindowHandleResolver { get; set; }
+
+        public bool IsRunning { get; private set; }
+
+        public bool ShutdownRequested => Volatile.Read(ref _shutdownRequested) != 0;
+
+        public bool Start()
+        {
+            ThrowIfDisposed();
+
+            if (IsRunning)
+            {
+                return true;
+            }
+
+            if (AutoLoadDependencies && !Dependencies.LoadDependencies(DependencyModuleHandle))
+            {
+                return false;
+            }
+
+            bool started = Renderer.GraphicsApi switch
+            {
+                GraphicsApi.DirectX9 => StartDirectX9(),
+                GraphicsApi.OpenGL => StartOpenGL(),
+                _ => throw new NotSupportedException($"{Renderer.GraphicsApi.ToDisplayName()} does not have an internal overlay host."),
+            };
+
+            IsRunning = started;
+            return started;
+        }
+
+        public bool Start(IntPtr dependencyModuleHandle)
+        {
+            DependencyModuleHandle = dependencyModuleHandle;
+            return Start();
+        }
+
+        public void RequestShutdown()
+        {
+            Interlocked.Exchange(ref _shutdownRequested, 1);
+        }
 
         public bool Initialize(nint device, nint windowHandle)
         {
@@ -46,8 +105,234 @@ namespace PhantomRender.ImGui
 
         public override void Dispose()
         {
+            if (_disposed)
+            {
+                return;
+            }
+
+            _disposed = true;
+            RequestShutdown();
+
+            DisposeHooks();
             Renderer.Dispose();
+            IsRunning = false;
+
             base.Dispose();
         }
+
+        private bool StartDirectX9()
+        {
+            var renderer = Renderer as DirectX9Renderer
+                ?? throw new InvalidOperationException("DirectX9 internal overlay requires a DirectX9Renderer.");
+
+            IntPtr deviceAddress = DirectX9Hook.GetDeviceAddress();
+            if (deviceAddress == IntPtr.Zero)
+            {
+                return false;
+            }
+
+            DX9HookFlags flags = DirectX9HookFlagsOverride ?? GetDirectX9HookFlags(renderer);
+            _directX9Hook = new DirectX9Hook(deviceAddress, flags);
+            _directX9Hook.OnBeforeReset += HandleDirectX9BeforeReset;
+            _directX9Hook.OnAfterReset += HandleDirectX9AfterReset;
+
+            if ((flags & DX9HookFlags.EndScene) != 0)
+            {
+                _directX9Hook.OnEndScene += HandleDirectX9EndScene;
+            }
+
+            if ((flags & DX9HookFlags.Present) != 0)
+            {
+                _directX9Hook.OnPresent += HandleDirectX9Present;
+            }
+
+            _directX9Hook.Enable();
+            return true;
+        }
+
+        private bool StartOpenGL()
+        {
+            if (!(Renderer is OpenGLRenderer))
+            {
+                throw new InvalidOperationException("OpenGL internal overlay requires an OpenGLRenderer.");
+            }
+
+            _openGLHook = new OpenGLHook();
+            _openGLHook.OnSwapBuffers += HandleOpenGLSwapBuffers;
+            _openGLHook.Enable();
+            return true;
+        }
+
+        private static DX9HookFlags GetDirectX9HookFlags(DirectX9Renderer renderer)
+        {
+            return renderer.InitializationEndpoint == DirectX9InitializationEndpoint.EndScene
+                ? DX9HookFlags.EndScene | DX9HookFlags.Reset
+                : DX9HookFlags.Present | DX9HookFlags.Reset;
+        }
+
+        private void HandleDirectX9Present(IntPtr device, IntPtr sourceRect, IntPtr destRect, IntPtr hDestWindowOverride, IntPtr dirtyRegion)
+        {
+            RenderDirectX9Frame(device, ResolveDirectX9WindowHandle(hDestWindowOverride));
+        }
+
+        private void HandleDirectX9EndScene(IntPtr device)
+        {
+            RenderDirectX9Frame(device, ResolveDirectX9WindowHandle(IntPtr.Zero));
+        }
+
+        private void RenderDirectX9Frame(IntPtr device, IntPtr windowHandle)
+        {
+            if (ShutdownRequested || windowHandle == IntPtr.Zero)
+            {
+                return;
+            }
+
+            if (!Renderer.IsInitialized && !Initialize(device, windowHandle))
+            {
+                return;
+            }
+
+            BeginFrame();
+            RenderFrame();
+        }
+
+        private void HandleDirectX9BeforeReset(IntPtr device, PhantomRender.Core.Native.Direct3D9.D3DPRESENT_PARAMETERS presentParameters)
+        {
+            if (!ShutdownRequested)
+            {
+                OnLostDevice();
+            }
+        }
+
+        private void HandleDirectX9AfterReset(IntPtr device, PhantomRender.Core.Native.Direct3D9.D3DPRESENT_PARAMETERS presentParameters)
+        {
+            if (!ShutdownRequested)
+            {
+                OnResetDevice();
+            }
+        }
+
+        private void HandleOpenGLSwapBuffers(IntPtr hdc)
+        {
+            if (ShutdownRequested)
+            {
+                return;
+            }
+
+            IntPtr windowHandle = WindowFromDC(hdc);
+            if (windowHandle == IntPtr.Zero)
+            {
+                windowHandle = ResolveWindowHandle();
+            }
+
+            if (windowHandle == IntPtr.Zero)
+            {
+                return;
+            }
+
+            if (!Renderer.IsInitialized && !Initialize(hdc, windowHandle))
+            {
+                return;
+            }
+
+            BeginFrame();
+            RenderFrame();
+        }
+
+        private IntPtr ResolveDirectX9WindowHandle(IntPtr hDestWindowOverride)
+        {
+            if (hDestWindowOverride != IntPtr.Zero && IsWindow(hDestWindowOverride))
+            {
+                return hDestWindowOverride;
+            }
+
+            return ResolveWindowHandle();
+        }
+
+        private IntPtr ResolveWindowHandle()
+        {
+            if (WindowHandleResolver != null)
+            {
+                IntPtr resolved = WindowHandleResolver();
+                if (resolved != IntPtr.Zero)
+                {
+                    return resolved;
+                }
+            }
+
+            try
+            {
+                IntPtr mainWindow = Process.GetCurrentProcess().MainWindowHandle;
+                if (mainWindow != IntPtr.Zero && IsWindow(mainWindow))
+                {
+                    return mainWindow;
+                }
+            }
+            catch
+            {
+            }
+
+            IntPtr foregroundWindow = GetForegroundWindow();
+            if (foregroundWindow != IntPtr.Zero && IsWindow(foregroundWindow))
+            {
+                return foregroundWindow;
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private void DisposeHooks()
+        {
+            if (_directX9Hook != null)
+            {
+                _directX9Hook.OnBeforeReset -= HandleDirectX9BeforeReset;
+                _directX9Hook.OnAfterReset -= HandleDirectX9AfterReset;
+                _directX9Hook.OnEndScene -= HandleDirectX9EndScene;
+                _directX9Hook.OnPresent -= HandleDirectX9Present;
+
+                try
+                {
+                    _directX9Hook.Dispose();
+                }
+                catch
+                {
+                }
+
+                _directX9Hook = null;
+            }
+
+            if (_openGLHook != null)
+            {
+                _openGLHook.OnSwapBuffers -= HandleOpenGLSwapBuffers;
+
+                try
+                {
+                    _openGLHook.Dispose();
+                }
+                catch
+                {
+                }
+
+                _openGLHook = null;
+            }
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(nameof(InternalOverlay));
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr WindowFromDC(IntPtr hdc);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
     }
 }
